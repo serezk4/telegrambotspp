d2966c084615b3bbc40947393972e977afc7032b
serezk4 <serezadorohin@gmail.com>
2023-11-15 01:22:20 +0300

    added Localization.java

--- src/main/java/com/serezka/Application.java
+++ src/main/java/com/serezka/Application.java
@@ -1,7 +1,7 @@
 package com.serezka;
 
 
-import com.serezka.telegram.api.SendMessage;
+import com.serezka.localization.Localization;
 import com.serezka.telegram.bot.Bot;
 import com.serezka.telegram.bot.Handler;
 import lombok.AccessLevel;
@@ -13,10 +13,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 import java.time.LocalDateTime;
-import java.util.Locale;
 
 @SpringBootApplication
 @RequiredArgsConstructor @Log4j2
@@ -32,13 +30,6 @@ public class Application implements ApplicationRunner {
     // ..
 
     public static void main(String[] args) {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("lang/messages");
-        messageSource.setDefaultLocale(Locale.of("us"));
-        messageSource.setDefaultEncoding("UTF-8");
-
-        System.out.println(messageSource.getMessage("hello", null, Locale.of("fr")));
-
         SpringApplication.run(Application.class, args);
     }
 
---
+++ src/main/java/com/serezka/localization/Localization.java
@@ -0,0 +1,50 @@
+package com.serezka.localization;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class Localization {
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @RequiredArgsConstructor @Getter
+    public enum Type {
+        RU("русский", Locale.of("ru")),
+        US("english", Locale.of("us"));
+
+        String name;
+        Locale locale;
+
+        public static final Type DEFAULT = Type.RU;
+    }
+
+    ResourceBundleMessageSource messageSource;
+
+    private Localization() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("lang/messages");
+        messageSource.setDefaultLocale(Locale.of("us"));
+        messageSource.setDefaultEncoding("UTF-8");
+
+        this.messageSource = messageSource;
+    }
+
+    private static Localization instance = null;
+
+    public static Localization getInstance() {
+        if (instance == null) instance = new Localization();
+        return instance;
+    }
+
+    public String get(String code, Type localization) {
+        return messageSource.getMessage(code, null, localization.getLocale());
+    }
+
+    public String get(String code) {
+        return messageSource.getMessage(code, null, Type.DEFAULT.getLocale());
+    }
+}
