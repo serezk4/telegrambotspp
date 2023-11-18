274fcd7f9f93f321e70b421fd28782c390153254
serezk4 <serezadorohin@gmail.com>
2023-11-18 05:31:05 +0300

    uncommented Handler.getHelp

--- src/main/java/com/serezka/telegram/bot/Handler.java
+++ src/main/java/com/serezka/telegram/bot/Handler.java
@@ -5,20 +5,18 @@ import com.serezka.database.service.UserService;
 import com.serezka.localization.Localization;
 import com.serezka.telegram.api.methods.SendMessage;
 import com.serezka.telegram.api.update.Update;
+import com.serezka.telegram.command.Command;
 import com.serezka.telegram.util.AntiSpam;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
-import org.telegram.telegrambots.meta.api.methods.ParseMode;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
-import java.util.WeakHashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Controller
@@ -27,8 +25,12 @@ import java.util.WeakHashMap;
 @PropertySource("classpath:telegram.properties")
 public class Handler {
     // handler per-init settings
-//    @Getter
-//    List<Command<? extends Session>> commands = new ArrayList<>();
+    @Getter
+    List<Command> commands = new ArrayList<>();
+
+    public void addCommand(Command command) {
+        commands.add(command);
+    }
 
     // database services
     UserService userService;
@@ -41,10 +43,6 @@ public class Handler {
 
     // cache
     Set<Long> authorized = Collections.newSetFromMap(new WeakHashMap<>());
-
-//    public void addCommand(Command<? extends Session> command) {
-//        commands.add(command);
-//    }
 
     public void process(Bot bot, Update update) {
         // check if user exists in database
@@ -90,15 +88,12 @@ public class Handler {
             userService.save(new User(update.getChatId(), update.getUsername()));
     }
 
-    @Deprecated // for this bot
-    public String getHelp(int adminLvl) {
-//        StringBuilder help = new StringBuilder("Кажется, вы ошиблись в команде. Список допустимых команд:\n");
-//        help.append(commands.stream()
-//                .filter(command -> command.getAdminLvl() <= adminLvl)
-//                .map(command -> String.format(" - <b>%s</b> - %s%n", command.getNames(), command.getHelp()))
-//                .collect(Collectors.joining()));
-//        return help.toString();
-
-        return "todo";
+    public String getHelp(User.Role userRole) {
+        StringBuilder help = new StringBuilder("Кажется, вы ошиблись в команде. Список допустимых команд:\n");
+        help.append(commands.stream()
+                .filter(command -> command.getRequiredRole().getAdminLvl() <= userRole.getAdminLvl())
+                .map(command -> String.format(" - <b>%s</b> - %s%n", command.getUsage(), command.getHelp()))
+                .collect(Collectors.joining()));
+        return help.toString();
     }
 }
