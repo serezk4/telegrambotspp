f946494080b57fe1647dc6cb1ff86d98282724c8
serezk4 <serezadorohin@gmail.com>
2023-12-04 16:51:55 +0300

    update sessions

--- src/main/java/com/serezka/telegram/session/Session.java
+++ src/main/java/com/serezka/telegram/session/Session.java
@@ -1,5 +1,8 @@
 package com.serezka.telegram.session;
 
+import com.serezka.telegram.api.meta.api.methods.BotApiMethod;
+import com.serezka.telegram.api.meta.api.methods.send.SendMessage;
+import com.serezka.telegram.api.meta.api.objects.Message;
 import com.serezka.telegram.api.meta.api.objects.Update;
 import com.serezka.telegram.bot.Bot;
 import lombok.AccessLevel;
@@ -8,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serializable;
 import java.util.*;
 
 @RequiredArgsConstructor
@@ -20,13 +24,35 @@ public abstract class Session {
     final Queue<Integer> botsMessagesIds = new PriorityQueue<>();
     final List<Integer> usersMessagesIds = new ArrayList<>();
     final long id = idCounter++;
-    @Getter @Setter boolean saveUsersMessages = true;
+    @Getter
+    @Setter
+    boolean saveUsersMessages = true;
 
     private final List<String> history = new LinkedList<>();
     private boolean created;
 
-    // generate answer
-    public abstract void init(Bot bot, Update update);
-    public abstract void next(Bot bot, Update update);
-    public abstract void destroy(Bot bot, Update update);
+    public void getNext(Bot bot, Update update) {
+        usersMessagesIds.add(update.getMessageId());
+
+        if (!created) {
+            init(bot, update);
+            created = true;
+        } else next(bot, update);
+    }
+
+    public <T extends Serializable, Method extends BotApiMethod<T>> T execute(Bot bot,Method method) {
+        if (method instanceof SendMessage sendMessage) {
+            Message result = bot.execute(sendMessage);
+            botsMessagesIds.add(result.getMessageId());
+
+            return (T) result;
+        }
+
+        return bot.execute(method);
+    }
+
+
+    protected abstract void init(Bot bot, Update update);
+    protected abstract void next(Bot bot, Update update);
+    protected abstract void destroy(Bot bot, Update update);
 }
