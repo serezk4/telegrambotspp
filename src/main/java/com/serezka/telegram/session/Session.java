e03e999fca23c9c25ccbc637e65f8fa638ee715d
serezk4 <serezadorohin@gmail.com>
2024-02-13 04:47:23 +0300

    continue working with sessions

--- src/main/java/com/serezka/telegram/session/Session.java
+++ src/main/java/com/serezka/telegram/session/Session.java
@@ -1,104 +1,67 @@
 package com.serezka.telegram.session;
 
 import com.serezka.telegram.bot.Bot;
-import com.serezka.telegram.session.step.Step;
 import com.serezka.telegram.util.Keyboard;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.tuple.Pair;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 
-import java.io.Serializable;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Getter @Log4j2
-public abstract class Session {
+public class Session {
     static int idCounter = 0;
 
-    // TODO fix this fucking shitcode
-
-    final Deque<Function<Update, Pair<String, ReplyKeyboard>>> stepDeque = new ArrayDeque<>();
+    Deque<BiConsumer<Bot, Update>> input = new ArrayDeque<>();
+    Deque<BiConsumer<Bot, Update>> trash = new ArrayDeque<>();
 
     Bot bot;
     long chatId;
+
+    // session variables
+    Queue<Integer> botsMessagesIds = new PriorityQueue<>();
+    List<Integer> usersMessagesIds = new ArrayList<>();
+    long id = idCounter++;
+    @NonFinal @Setter boolean saveUsersMessages = true;
+
+    List<String> history = new LinkedList<>();
 
     public Session(Bot bot, long chatId) {
         this.bot = bot;
         this.chatId = chatId;
     }
 
-    // in-line creation
-    public Session send(Function<Update, Pair<String, ReplyKeyboard>> function) {
-        stepDeque.add(function);
-        return this;
-    }
-
-    public Session send(String text, ReplyKeyboard replyKeyboard) {
-
-        return this;
-    }
-
-    public Session send(String text) {
-        send(text, Keyboard.Reply.DEFAULT);
-        return this;
-    }
-
-    public Session get(String text, ReplyKeyboard replyKeyboard) {
-        bot.executeAsync(SendMessage.builder().text(text).chatId(chatId).replyMarkup(replyKeyboard).build());
-        return this;
-    }
-
-    public Session get(String text) {
-        get(text, Keyboard.Reply.DEFAULT);
-        return this;
-    }
-
-    public Session getWhileMatches(String text, ReplyKeyboard replyKeyboard, Function<Update, Boolean> function) {
-        // todo
-        return this;
-    }
-
-    public Session getWhileMatches(String text, Function<Update, Boolean> function) {
-        getWhileMatches(text, Keyboard.Reply.DEFAULT, function);
-        return this;
-    }
-
-    public Session rollbackIf(Function<Update, Boolean> function) {
-        // todo
-        return this;
-    }
-
-    // session variables
-    private final Queue<Integer> botsMessagesIds = new PriorityQueue<>();
-    private final List<Integer> usersMessagesIds = new ArrayList<>();
-    final long id = idCounter++;
-    @Setter boolean saveUsersMessages = true;
-
-    private final List<String> history = new LinkedList<>();
-    private boolean created;
-
+    /**
+     * get next step
+     * @param bot - bot
+     * @param update - update
+     */
     public void next(Bot bot, Update update) {
         usersMessagesIds.add(update.getMessageId());
-
-        if (!created) {
-            init(bot, update);
-            created = true;
-        } else getNext(bot, update);
+        getNext(bot, update);
     }
 
-    protected abstract void init(Bot bot, Update update);
-    protected abstract void getNext(Bot bot, Update update);
+    protected void getNext(Bot bot, Update update) {
+        if (input.isEmpty()) {
+            log.warn("Session {} has no input", id);
+            destroy(bot, update);
+            return;
+        }
+
+        trash.add(input.pop());
+        Objects.requireNonNull(trash.peek()).accept(bot, update);
+    }
+
     protected void destroy(Bot bot, Update update) {
         // delete users messages
         if (!saveUsersMessages) usersMessagesIds.forEach(
@@ -110,4 +73,51 @@ public abstract class Session {
         // todo add menu manager instance here
 
     };
+
+    // in-line creation
+    public Session send(BiConsumer<Bot, Update> function) {
+        input.add(function);
+        return this;
+    }
+
+    public Session send(String text, ReplyKeyboard replyKeyboard) {
+        input.add((bot, update) -> bot.executeAsync(SendMessage.builder()
+                .text(text).chatId(chatId)
+                .replyMarkup(replyKeyboard)
+                .build()));
+        return this;
+    }
+
+    public Session send(String text) {
+        send(text, Keyboard.Reply.DEFAULT);
+        return this;
+    }
+
+    public Session get(String text, ReplyKeyboard replyKeyboard) {
+        bot.executeAsync(SendMessage.builder()
+                .text(text).chatId(chatId)
+                .replyMarkup(replyKeyboard)
+                .build());
+        return this;
+    }
+
+    public Session get(String text) {
+        get(text, Keyboard.Reply.DEFAULT);
+        return this;
+    }
+
+    public Session tryGet(String text, ReplyKeyboard replyKeyboard, Function<Update, Boolean> function) {
+        // todo
+        return this;
+    }
+
+    public Session tryGet(String text, Function<Update, Boolean> function) {
+        tryGet(text, Keyboard.Reply.DEFAULT, function);
+        return this;
+    }
+
+    public Session rollbackIf(Function<Update, Boolean> function) {
+        // todo
+        return this;
+    }
 }
